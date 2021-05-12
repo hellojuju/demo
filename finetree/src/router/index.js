ea92e85eafbc003c3206a774aa9a19872df9bf52
youngju <hellodq7700@gmail.com>
2021-05-12 22:51:46 +0900

    vue demo commit

---
+++ finetree/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView';
+import ItemView from '../views/ItemView';
+import MyPageView from '../views/MyPageView';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path : '/',
+            component : HomeView,
+        },
+         {
+            path : '/item',
+            component : ItemView,
+        },
+         {
+            path : '/mypage',
+            component : MyPageView,
+        },
+    ]
+});
+
---
+++ finetree/src/views/ItemView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Item
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
